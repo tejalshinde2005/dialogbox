94a1bc681a042432adc78febfc4097d4a1331082
tejalshinde2005 <shindetejal2005@gmail.com>
2024-12-17 19:29:57 +0530

    create application

---
+++ Desktop/dilogbox/js/script.js
@@ -0,0 +1,9 @@
+function openModal(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display='flex';
+}
+
+function closeModal(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display='none';
+}
